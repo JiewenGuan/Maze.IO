221aa2d0529af0a03c1c4e4b2823cc8927ba643c
诚实肥宅黑夜 <heiye1996@outlook.com>
2022-05-05 05:12:18 +0800

    animation and speed

--- scripts/game.js
+++ scripts/game.js
@@ -11,7 +11,7 @@ import {
 import { OrbitControls } from "./orbit.js";
 import { FontLoader } from "./fontLoader.js";
 import { GUI } from "./lilgui.js";
-
+import { sleep } from "./utils.js";
 export class MyCam extends PerspectiveCamera {
     constructor(zdistance = 30) {
         super(75, window.innerWidth / window.innerHeight, 0.1, 2000);
@@ -46,12 +46,24 @@ export class MyScene extends Scene {
         this.add(this.lights[0]);
         this.add(this.lights[1]);
         this.add(this.lights[2]);
+    }
+}
 
-        //test cube
-        let geometry = new BoxGeometry(1, 1, 1);
-        let material = new MeshPhongMaterial({ color: 0x00ff00 });
-        let cube = new Mesh(geometry, material);
-        this.add(cube);
+export class MyController extends GUI {
+    constructor(game) {
+        super({ title: "Settings" });
+        this.game = game;
+        this.data = {
+            //maze
+            Width: 10,
+            Height: 10,
+            Seed: Math.floor(Math.random() * Number.MAX_SAFE_INTEGER),
+            delayMs: 10,
+            direction: 0,
+        };
+        this.add(this.data, "delayMs", 1, 1000, 1);
+
+        this.config = this.add(this.data, "direction", 0, 5, 1);
     }
 }
 
@@ -62,7 +74,8 @@ export class Game {
         this.renderer = new MyRenderer();
         this.scene = new MyScene();
         this.orbit = new OrbitControls(this.camera, this.renderer.domElement);
-        this.gui = new GUI({ title: "Config" });
+        this.gui = new MyController(this);
+        this.lastMove = new Date().getTime();
 
         window.addEventListener(
             "resize",
@@ -77,10 +90,39 @@ export class Game {
         let textLoad = new FontLoader().load("font.json", function (font) {
             game.font = font;
         });
+
+        //test cube
+        let geometry = new BoxGeometry(1, 1, 1);
+        let material = new MeshPhongMaterial({ color: 0x00ff00 });
+        this.cube = new Mesh(geometry, material);
+        this.scene.add(this.cube);
+    }
+    animate() {
+        let now = new Date().getTime();
+        let delta = now - this.lastMove;
+        if (delta > this.gui.data.delayMs) {
+            this.lastMove = new Date().getTime();
+            switch (this.gui.data.direction) {
+                case 0:
+                    break;
+                case 1:
+                    this.cube.position.y += 0.1;
+                    break;
+                case 2:
+                    this.cube.position.x += 0.1;
+                    break;
+                case 3:
+                    this.cube.position.y -= 0.1;
+                    break;
+                case 4:
+                    this.cube.position.x -= 0.1;
+                    break;
+            }
+        }
     }
     render() {
         requestAnimationFrame(game.render);
-
+        game.animate();
         game.renderer.render(game.scene, game.camera);
     }
 }
