4d2152292de0bcd55a1dee8e39479372078e44e7
诚实肥宅黑夜 <heiye1996@outlook.com>
2022-05-07 22:39:28 +0800

    patch

--- scripts/games.js
+++ scripts/games.js
@@ -1,9 +1,4 @@
-import {
-    Mesh,
-    MeshPhongMaterial,
-    BoxGeometry,
-    SphereGeometry,
-} from "./three.js";
+import {Maze} from './models.js';
 class Game {
     constructor(base) {
         this.scene = base.scene;
@@ -23,6 +18,24 @@ class Game {
 export class MazeGame extends Game {
     constructor(base) {
         super(base);
+        document.getElementById("MESSAGE").innerHTML = "Move with wasd or arrow keys";
+        this.buildController();
+        this.generate();
+    }
+    generate() {
+        this.maze = new Maze(this.data.config);
+
+    }
+    start(){
+        alert("start")
+    }
+    stop() {
+        alert("stop")
+    }
+    reset(){
+        alert("reset")
+    }
+    buildController() {
         this.data = {
             config: {
                 width: 10,
@@ -39,12 +52,15 @@ export class MazeGame extends Game {
                 pathLength: 0,
             },
             methods: {
-                generate: this.generate.bind(this),}
+                generate: this.generate.bind(this),
+                start: this.start.bind(this),
+                stop: this.stop.bind(this),
+                reset: this.reset.bind(this),}
         };
         this.config = this.addFolder("Config");
-        this.config.add(this.data.config, "width", 1, 50, 1).name("Width");
-        this.config.add(this.data.config, "height", 1, 50, 1).name("Height");
-        this.config.add(this.data.config, "seed", 1, 50, 1).name("Random Seed");
+        this.config.add(this.data.config, "width", 5, 50, 1).name("Width");
+        this.config.add(this.data.config, "height", 5, 50, 1).name("Height");
+        this.config.add(this.data.config, "seed").name("Random Seed");
         this.config
             .add(this.data.config, "walls", ["default", "loop", "noWall"])
             .name("Walls");
@@ -60,21 +76,21 @@ export class MazeGame extends Game {
         this.config
             .add(this.data.config, "numberOfGoals", 1, 5, 1)
             .name("Number of Goals");
+            this.config.add(this.data.methods, "generate");
+
         this.statics = this.addFolder("Statics");
         this.statics.add(this.data.statics, "steps").name("Steps").listen();
         this.statics.add(this.data.statics, "pathLength").name("Path Length").listen();
-        this.addCtrl(this.data.methods, "generate");
-    }
-    generate() {
-        alert(this.data.config.width)
+        this.control = this.addFolder("Control");
+        this.control.add(this.data.methods, "start");
+        this.control.add(this.data.methods, "stop");
+        this.control.add(this.data.methods, "reset");
+
     }
 }
 export class RubicsCubeGame extends Game {
     constructor(base) {
         super(base);
-        let geometry = new SphereGeometry();
-        let material = new MeshPhongMaterial({ color: 0xff0000 });
-        let cube = new Mesh(geometry, material);
-        this.addItem(cube);
+        document.getElementById("MESSAGE").innerHTML = "Rubics Cube is under construction";
     }
 }
--- scripts/models.js
+++ scripts/models.js
@@ -1,3 +1,30 @@
-export class Maze{
-    constructor()
+import {
+    Vector2,
+    Vector3,
+} from "./three.js";
+import { Random } from "./utils.js";
+export class Maze {
+    constructor(config) {
+        this.width = config.width;
+        this.height = config.height;
+        this.random = new Random(config.seed);
+        this.walls = config.walls;
+
+        this.start = [];
+        for (let i = 0; i < config.numberOfStarts; i++) {
+            this.start.push(this.makeVector(true))
+        }
+        this.goal = [];
+        for(let i = 0; i < config.numberOfGoals; i++){
+            this.goal.push(this.makeVector(false))
+        }
+        this.map = [];
+    }
+    makeMaze(){
+        let fa = {}
+        
+    }
+    makeVector(isStart){
+
+    }
 }
